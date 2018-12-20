11b3566ea2c1d96380013dd0c6ad6eeaa7e509ac
雪霁 <wang.haiguang@puscene.com>
2018-12-20 14:02:47 +0800

    .

--- src/url.ts
+++ src/url.ts
@@ -1,15 +1,17 @@
 const defaultPatterns = [
-    /"(https?:)?\/\/[\w\$\-\_\.\+\!\*\'\(\)\,\;\/\?\:\@\=\&]+"/g,
+    /(?:")(https?:)?\/\/[\w\$\-\_\.\+\!\*\'\(\)\,\;\/\?\:\@\=\&]+(?=")/g,
 ];
 
-export function extract(document: string, patterns: Array<RegExp> = []): Array<string> {
-    const urls = patterns
-                    .concat(defaultPatterns)
-                    .map(reg => document.match(reg))
-                    .reduce((acc: Array<string>, cur) => {
-                        cur && (acc = acc.concat(cur))
-                        return acc
-                    }, []) as Array<string>;
+export function extract(document: string, patterns: Array<RegExp> = defaultPatterns): Array<string> {
+    const urls =
+        patterns
+            .map(reg => document.match(reg))
+            .reduce((acc: Array<string>, cur) => {
+                cur && (acc = acc.concat(cur))
+                return acc
+            }, [])
+            .map(matches => matches.replace('"', ''))
+            .map(url => url.replace(/^(?=\/\/)/, 'http:'));
 
     return urls;
 }
