4a87a0d7c8a39a354d557a0f4f879720ee02c06b
雪霁 <vatel@Xor.local>
2019-03-19 12:10:55 +0800

    change fetcher behavior

--- src/fetcher.ts
+++ src/fetcher.ts
@@ -5,23 +5,19 @@ import { EventEmitter } from 'events';
 import { URLObject, parse } from './url';
 
 export interface FetcherOptions extends https.RequestOptions {
-    
+
 }
 
 export default class Fetcher extends EventEmitter {
     public url: URLObject;
 
-    request: http.ClientRequest;
+    request!: http.ClientRequest;
 
-    response?: http.IncomingMessage;
+    response!: http.IncomingMessage;
 
     options?: FetcherOptions;
 
-    buffer: Buffer[] = [];
-
-    fetchCalled: Boolean = false;
-
-    errorBuffer: Error[] = [];
+    private buffer: Buffer[] = [];
 
     constructor(url: string | URL | URLObject, options?: FetcherOptions) {
         super();
@@ -29,10 +25,35 @@ export default class Fetcher extends EventEmitter {
         this.url = parse(url);
         this.options = options;
 
+        this.on('request', () => {
+            this.request!
+                .on('response', (response: http.IncomingMessage) => {
+                    this.response = response;
+                    this.emit('response');
+                })
+                .on('error', (error) => {
+                    this.request!.abort();
+                    this.emit('error', error);
+                });
+        })
+
+        this.on('response', () => {
+            this.response!
+                .on('data', (chunk: Buffer) => {
+                    this.buffer.push(chunk);
+                })
+                .on('end', this.onResponseEnd.bind(this))
+                .on('error', (error) => {
+                    this.emit('error', error);
+                });
+        });
+    }
+
+    public async fetch(): Promise<Buffer> {
         const requestOptions: https.RequestOptions = Object.assign(
             {},
             this.url,
-            options,
+            this.options,
         );
 
         switch (requestOptions.protocol) {
@@ -43,34 +64,7 @@ export default class Fetcher extends EventEmitter {
                 this.request = http.request(requestOptions);
         }
 
-        this.request
-        .on('response', (response: http.IncomingMessage) => {
-            this.response = response;
-            this.emit('response');
-        })
-        .on('error', (error) => {
-            this.request.abort();
-            this.fetchCalled && this.emit('error', error) || this.errorBuffer.push(error);
-        });
-
-        this.on('response', () => {
-            this.response!
-            .on('data', (chunk: Buffer) => {
-                this.buffer.push(chunk);
-            })
-            .on('end', this.onResponseEnd.bind(this))
-            .on('error', (error) => {
-                this.fetchCalled && this.emit('error', error) || this.errorBuffer.push(error);
-            });
-        });
-    }
-
-    public async fetch(): Promise<Buffer> {
-        this.fetchCalled = true;
-        
-        this.errorBuffer.forEach(error => {
-            this.emit('error', error);
-        });
+        this.emit('request');
 
         this.request.end();
 
@@ -81,7 +75,7 @@ export default class Fetcher extends EventEmitter {
 
             this.on('error', (error) => {
                 reject(error);
-                this.request.abort();
+                this.request!.abort();
             });
         });
     }
@@ -110,3 +104,19 @@ export default class Fetcher extends EventEmitter {
         this.emit('end', Buffer.concat(this.buffer));
     }
 }
+
+if (require.main === module) 
+(async () => {
+    const fetcher = new Fetcher('http://sports.sina.com.cn/');
+
+    console.log(fetcher.request, fetcher.response);
+
+    const result = fetcher.fetch();
+
+    console.log(fetcher.request.constructor.name, fetcher.response);
+
+    await result;
+
+    console.log(fetcher.request.constructor.name, fetcher.response.constructor.name);
+})()
+
