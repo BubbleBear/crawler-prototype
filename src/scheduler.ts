cff3aa02bb2546bbe9a39f6662caed381b4037a5
雪霁 <vatel@Xor.local>
2019-04-01 20:30:33 +0800

    .

--- src/scheduler.ts
+++ src/scheduler.ts
@@ -45,31 +45,46 @@ export default class Scheduler extends EventEmitter {
         while (!this.runningTasks.empty()) {
             this.runningTasks.forEach(node => node.value.status !== TaskStatus.running && this.runTask(node.value));
 
+            console.log('running before dispatch', this.runningTasks.length)
+
             await new Promise((resolve) => {
                 this.once('dispatch', resolve);
             });
+
+            console.log(4)
+
+            console.log('running after dispatch', this.runningTasks.length)
         }
     }
 
     private schedule(offset: number = 0) {
+        console.log(1)
         this.runningTasks.forEach(node => (node.value.status === TaskStatus.running) || node.delete());
 
+        console.log('pending before schedule', this.pendingTasks.length)
+        // console.log(this.runningTasks.length)
+
         const todoTasks = this.pendingTasks.splice(offset, this.parallelSize - this.runningTasks.length);
+
+        console.log('todo', todoTasks.length)
+        console.log('pending after schedule', this.pendingTasks.length)
 
         todoTasks.forEach(node => this.runningTasks.push(node.value));
     }
 
     private async runTask(task: Task) {
+        console.log(2)
         task.status = TaskStatus.running;
 
         try {
-            task.status = TaskStatus.done;
             const result = await task.do();
+            task.status = TaskStatus.done;
+            console.log(3)
 
             await this.onDone(result, task);
         } catch (error) {
-            task.status = TaskStatus.failed;
             task.error = error;
+            task.status = TaskStatus.failed;
 
             await this.onError(error, task);
         }
@@ -111,4 +126,37 @@ export default class Scheduler extends EventEmitter {
             newTask: this.newTask = this.newTask,
         } = options as any);
     }
+}
+
+if (require.main === module) {
+    !async function() {
+        const schd = new Scheduler({
+            parallelSize: 10,
+            newTask(n) {
+                return {
+                    status: TaskStatus.pending,
+                    do: async () => await n,
+                };
+            },
+            async onDone(n, task) {
+                // console.log('pending length: ', schd.pendingTasks.length)
+                // console.log('running length: ', schd.runningTasks.length)
+                // console.log('failed length: ', schd.failedTasks.length)
+
+                schd.pendingTasks.push(schd.newTask(n + 10));
+
+                await new Promise(r => {
+                    setTimeout(() => {
+                        r();
+                    }, 1000);
+                })
+            }
+        });
+    
+        Array(20).fill(0).forEach((_, k) => {
+            schd.pendingTasks.push(schd.newTask(k));
+        });
+    
+        await schd.dispatch();
+    }()
 }
